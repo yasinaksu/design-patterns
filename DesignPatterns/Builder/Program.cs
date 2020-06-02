c9163e522160d5c007028382a8e6779980a932ef
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-02 12:17:44 +0300

    builder design pattern completed.

---
+++ DesignPatterns/Builder/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector productDirector = new ProductDirector();
+            var builder = new NewCustomerProductBuilder();
+            productDirector.GenerateProduct(builder);
+            var model = builder.GetModel();
+
+            Console.WriteLine("Name: {0} Price : {1} DiscountedPrice: {2}", model.ProductName, model.UnitPrice, model.DiscountedPrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscount();
+
+        public abstract ProductViewModel GetModel();
+    }
+
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice * 0.90M;
+            model.DiscountApplied = true;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverage";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+        }
+    }
+
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice;
+            model.DiscountApplied = false;
+        }
+
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverage";
+            model.ProductName = "Chai";
+            model.UnitPrice = 20;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.ApplyDiscount();
+        }
+    }
+}
