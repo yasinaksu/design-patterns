f1a189b353e8a24ddbdf220e84c4cd2c33fa4b2f
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-02 11:52:45 +0300

    prototype finished.

--- DesignPatterns/Prototype/Program.cs
+++ DesignPatterns/Prototype/Program.cs
@@ -10,6 +10,40 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
+            Customer customer1 = new Customer { City = "Ankara", FirstName = "Yasin", LastName = "Anything", Id = 1 };
+            Console.WriteLine("customer 1 firstname: "+customer1.FirstName);
+
+            Customer customer2 = (Customer)customer1.Clone();
+            customer2.FirstName = "Emre";
+            Console.WriteLine("customer 1 firstname (after clone): " + customer1.FirstName);
+            Console.WriteLine("customer 2 firstname: " + customer2.FirstName);
+            Console.ReadLine();
+        }
+    }
+
+    public abstract class Person
+    {
+        public abstract Person Clone();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer : Person
+    {
+        public string City { get; set; }
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
+        }
+    }
+
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+        public override Person Clone()
+        {
+            return (Person)MemberwiseClone();
         }
     }
 }
