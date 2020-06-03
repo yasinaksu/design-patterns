382906347436a525749fb27ab7d86f92c8723ff6
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-03 11:50:54 +0300

    proxy completed

--- DesignPatterns/Proxy/Program.cs
+++ DesignPatterns/Proxy/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Proxy
@@ -10,6 +11,44 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
+            CreditManagerProxy proxxy = new CreditManagerProxy();
+            Console.WriteLine(proxxy.Calculate());
+            Console.WriteLine(proxxy.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+    abstract class CreditBase
+    {
+        public abstract int Calculate();
+    }
+
+    class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+            return result;
+        }
+    }
+
+    class CreditManagerProxy : CreditBase
+    {
+        private CreditManager _creditManager;
+        private int _cachedValue;
+        public override int Calculate()
+        {
+            if (_creditManager==null)
+            {
+                _creditManager = new CreditManager();
+                _cachedValue = _creditManager.Calculate();
+            }
+            return _cachedValue;
         }
     }
 }
