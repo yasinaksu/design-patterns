b9e9d8c8c4f5bdfa2f6299e9b14b5309e3b48e28
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-04 16:06:48 +0300

    observer design pattern completed commit

---
+++ DesignPatterns/Observer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager();
+            productManager.Attach(new CustomerObserver());
+            productManager.Attach(new EmployeeObserver());
+            productManager.UpdatePrice();
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductManager
+    {
+        List<Observer> _observers = new List<Observer>();
+        public void UpdatePrice()
+        {
+            Console.WriteLine("Product price updated.");
+            Notify();
+        }
+        public void Attach(Observer observer)
+        {
+            _observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    class CustomerObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("attention customer product price changed.");
+        }
+    }
+    class EmployeeObserver : Observer
+    {
+        public override void Update()
+        {
+            Console.WriteLine("attention employee product price changed.");
+        }
+    }
+}
