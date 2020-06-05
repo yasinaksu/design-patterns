45f6b8337c41a69f879d096078d4f39c8a904fc4
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-05 11:35:47 +0300

    state pattern comleted commit

---
+++ DesignPatterns/State/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            ModifiedState modifiedState = new ModifiedState();
+            modifiedState.DoAction(context);
+            Console.WriteLine(context.GetState().ToString());
+
+            Console.ReadLine();
+        }
+    }
+    class Context
+    {
+        private IState _state;
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+        public IState GetState()
+        {
+            return _state;
+        }
+    }
+    interface IState
+    {
+        void DoAction(Context context);
+    }
+
+    class ModifiedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("state : modified");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Modified";
+        }
+    }
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("state : deleted");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Deleted";
+        }
+    }
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("state : added");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Added";
+        }
+    }
+}
