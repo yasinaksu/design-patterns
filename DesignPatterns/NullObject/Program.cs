1db430c7762ac0f1ae594774c17375e05c154518
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-05 16:15:58 +0300

    null object pattern completed commit.

---
+++ DesignPatterns/NullObject/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new NLogLogger());
+            customerManager.Save();
+
+            CustomerManagerTests test = new CustomerManagerTests();
+            test.SaveTest();
+
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        private readonly ILogger _logger;
+
+        public CustomerManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved");
+            _logger.Log();
+        }
+    }
+    interface ILogger
+    {
+        void Log();
+    }
+    class Log4NetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with Log4Net");
+        }
+    }
+    class NLogLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with NLog");
+        }
+    }
+    class StubLogger : ILogger
+    {
+        private static StubLogger _stubLogger;
+        private static object _lock = new object();
+        private StubLogger()
+        {
+
+        }
+        public static StubLogger GetStubLogger()
+        {
+            lock (_lock)
+            {
+                if (_stubLogger==null)
+                {
+                    _stubLogger = new StubLogger();
+                }
+            }
+            return _stubLogger;
+        }
+        public void Log()
+        {
+            Console.WriteLine("Moc logger invoked.");
+        }
+    }
+    class CustomerManagerTests
+    {
+        public void SaveTest()
+        {
+            CustomerManager customerManager = new CustomerManager(StubLogger.GetStubLogger());
+            customerManager.Save();
+            Console.WriteLine("Test succed");
+        }
+    }
+}
