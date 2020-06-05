808dc5d8a3e96d92bf8930787ffc02c4b6863f18
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-05 16:00:29 +0300

    DI pattern completed commit

---
+++ DesignPatterns/DependencyInjection/Program.cs
@@ -0,0 +1,56 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+            //ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+
+
+            Console.ReadLine();
+        }
+    }
+    interface IProductDal
+    {
+        void Save();
+    }
+    class EfProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("saved with ef");
+        }
+    }
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("saved with Nh");
+        }
+    }
+    class ProductManager
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
